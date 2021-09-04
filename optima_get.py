0301df341fa87c5f23cca5fcb03017ed7e7e1c3a
Artem Lukin <artemlukin15@gmail.com>
2021-09-04 08:04:37 +0300

    minor fixes

--- optima_get.py
+++ optima_get.py
@@ -1,29 +1,39 @@
 # -*- coding: utf-8 -*-
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Union
 import requests
 import os
 import pathlib
 from bs4 import BeautifulSoup as Soup
 import re
+import sys
+import traceback
 
-firefox_header = ""
-
-
-def firefox_headers_to_dict(raw_header: str) -> Tuple[str, Dict]:
-    raw_pairs = raw_header.split('\n')
-    pairs = raw_pairs[1:] # Removes the request
-    header_dictionary = {pair.partition(':')[0]: pair.partition(':')[2].strip() for pair in pairs}
-
-    request = raw_pairs[0]
-    url = header_dictionary.get('Host') + request.split()[1]
-    return (url, header_dictionary)
+try:
+    from header_config import header as user_header
+except ImportError as e:
+    print(f"ImportError: Failed to import header! Maybe you haven't renamed 'template_header_config.py' to 'header_config.py'?\nFull error message: {e}")
+    sys.exit(-1)
 
 
+def firefox_headers_to_dict(raw_header: str) -> Union[Tuple[str, Dict], Dict]:
+    try:
+        raw_pairs = raw_header.strip().split('\n')
+        pairs = raw_pairs[1:] # Removes the request
+        header_dictionary = {pair.partition(':')[0].strip(): pair.partition(':')[2].strip() for pair in pairs}
 
-    
+        request = raw_pairs[0]
+        if host := header_dictionary.get('Host'): 
+            url = host + request.split()[1] 
+            return (url, header_dictionary)
+        else: 
+            url = None
+            return header_dictionary
+    except Exception:
+        print(f"Something went wrong with parsing user header data! Make sure you pasted it correctly.\nFull exception:\n\n")
+        print(traceback.format_exc())
+        sys.exit(-1)
 
-os.chdir(pathlib.Path().resolve())
-headers = {}
+
 def write_to_html(url: str, headers: dict, file: str, joining = '') -> None:
     received_html = requests.get(url, headers=headers)
     with open(file, 'a', encoding='utf-8') as f:
@@ -33,9 +43,5 @@ def write_to_html(url: str, headers: dict, file: str, joining = '') -> None:
 
 
 if __name__ == '__main__':
-    # for id in range(196588, 196600):
-    #     write_to_html(f'https://b.optima-osvita.org/mod/lesson/view.php?id=47586&pageid={id}', 
-    #                     headers, 
-    #                     "bruh.html", 
-    #                     f"\n\n\n\n===={id}====\n\n\n\n")
-    print(firefox_headers_to_dict(firefox_header))
+    os.chdir(pathlib.Path().resolve())
+    print(firefox_headers_to_dict(user_header))
